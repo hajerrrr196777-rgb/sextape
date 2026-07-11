76b88ad0c4dac37090c1974de123ca01e833fe56
hajerrrr196777-rgb <hajerrrr196777@gmail.com>
2026-07-11 09:34:03 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "h",
+        name: "angeleeyes",
         // Activity type number (0 = Playing).
         type: 2,
       },
